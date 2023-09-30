471c8c943d1ba92be63fa994150546bb360aca1c
Carl Colburn <carlcolburn@gmail.com>
2023-09-30 11:01:36 -0700

    File read in, can navigate it on console

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # belly-button-challenge
 Module 14 homework
+1. // use D3 library to read in JSON (provided link in "Instructions" on bootcampspot)
+    solution is modeled after instructor post in Slack (Edward Apostol):
+    "demo of using d3's json() method to retrieve data from a URL."
+
+
---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+// use D3 library to read in JSON (provided link in "Instructions" on bootcampspot)
+
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
+
+d3.json(url).then(function(data) {
+    console.log(' the results of making this API call is = \n', data);
+  });
