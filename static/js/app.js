7e324d80cb5579aee866003ec18bea2896a291bc
Carl Colburn <carlcolburn@gmail.com>
2023-09-30 15:09:31 -0700

    1

--- static/js/app.js
+++ static/js/app.js
@@ -7,4 +7,23 @@ console.log("Data Promise: ", dataPromise);
 
 d3.json(url).then(function(data) {
     console.log(' the results of making this API call is = \n', data);
-  });
+    //init_button(data)
+});
+
+//Variables needed to hold values and initialize charts
+//repeat line above to get to the data again.
+var samples;
+var meta_data;
+d3.json(url).then(function (data) {
+    let selector = d3.select("#selDataset"); //"selDataset" is predefined name of dropdown
+    meta_data = data.metadata;
+    samples = data.samples;
+    data.names.forEach((id) => {
+        selector.append("option").text(id).property("value", id);
+    });
+    //To initialize charts, need values from first data row. Here we populate them
+    metaData(meta_data[0]);
+    barChart(samples[0]);
+    bubbleChart(samples[0]);
+});
+
