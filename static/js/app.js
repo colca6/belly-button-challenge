4b94ef6b5e18e5f6cbc7de0e4192c7d24b995cd9
Carl Colburn <carlcolburn@gmail.com>
2023-10-01 17:14:22 -0700

    Sunday afternoon

--- static/js/app.js
+++ static/js/app.js
@@ -15,15 +15,107 @@ d3.json(url).then(function(data) {
 var samples;
 var meta_data;
 d3.json(url).then(function (data) {
-    let selector = d3.select("#selDataset"); //"selDataset" is predefined name of dropdown
+    let dropdown = d3.select("#selDataset"); //"selDataset" is predefined dropdown selector
     meta_data = data.metadata;
     samples = data.samples;
+    //"option" comes from "selDataset", the values are the ids
     data.names.forEach((id) => {
-        selector.append("option").text(id).property("value", id);
+        dropdown.append("option").text(id).property("value", id);
     });
     //To initialize charts, need values from first data row. Here we populate them
     metaData(meta_data[0]);
     barChart(samples[0]);
     bubbleChart(samples[0]);
 });
+
+
+
+function metaData(demographicInfo) {
+    let x = d3.select("#sample-metadata");
+//these items come from <h3 class="panel-title">Demographic Info</h3>
+    x.html(
+    `id: ${demographicInfo.id} <br> 
+    ethnicity: ${demographicInfo.ethnicity} <br>
+    gender: ${demographicInfo.gender} <br>
+    age: ${demographicInfo.age} <br>
+    location: ${demographicInfo.location} <br>
+    bbtype: ${demographicInfo.bbtype} <br>
+    wfreq: ${demographicInfo.wfreq}`
+    );
+}
+
+function barChart(selectedId) {
+    let x_axis = selectedId.sample_values.slice(0, 10).reverse();
+    let y_axis = selectedId.otu_ids
+        .slice(0, 10)
+        .reverse()
+        .map((item) => `OTU ${item}`);
+    let text = selectedId.otu_labels.slice(0, 10).reverse();
+
+    barChart = {
+        x: x_axis,
+        y: y_axis,
+        text: text,
+        type: "bar",
+        orientation: "h",
+    };
+
+    let chart = [barChart];
+        
+    let layout = {
+        margin: {
+            l: 100,
+            r: 100,
+            t: 0,
+            b: 100,
+        },
+        height: 500,
+        width: 600,
+    };
+
+    Plotly.newPlot("bar", chart, layout);
+}
+
+
+function bubbleChart(selectedId) {
+    let x_axis = selectedId.otu_ids;
+    let y_axis = selectedId.sample_values;
+    let marker_size = selectedId.sample_values;
+    let color = selectedId.otu_ids;
+    let text = selectedId.otu_labels;
+
+    bubble = {
+        x: x_axis,
+        y: y_axis,
+        text: text,
+        mode: "markers",
+        marker: {
+            color: color,
+            colorscale: "Pastel",
+            size: marker_size,
+        },
+        type: "scatter",
+    };
+    let chart = [bubble];
+
+    let layout = {
+        xaxis: {
+            title: { text: "OTU ID" },
+        },
+    };
+    Plotly.newPlot("bubble", chart, layout);
+}
+function optionChanged(value) {
+    let selectedId = samples.find((item) => item.id === value);
+    let demographicInfo = meta_data.find((item) => item.id == value);
+
+    // Insterting Demographic Data using metaData function
+    metaData(demographicInfo);
+    // Call Bar Chart function
+    barChart(selectedId);
+    // Call Bubble Chart function
+    bubbleChart(selectedId);
+}
+
+
 
